5fcfca552eda83461261df0b0012e470b06918ca
0xbe1 <0xbetrue@gmail.com>
2023-08-14 21:00:07 +0800

    use vanilla aptos sdk

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
-import { createClient } from '@thalalabs/surf';
-import { COIN_ABI } from './abi';
+import { AptosClient } from 'aptos';
 
 async function main() {
-    const client = createClient({
-        nodeUrl: 'https://fullnode.mainnet.aptoslabs.com/v1',
-    });
-    const [supply] = await client.useABI(COIN_ABI).view.supply({
-        type_arguments: ['0x1::aptos_coin::AptosCoin'],
+    const client = new AptosClient('https://fullnode.mainnet.aptoslabs.com/v1');
+    const supply = await client.view({
+        function: "0x1::coin::supply",
+        type_arguments: ["0x1::aptos_coin::AptosCoin"],
         arguments: []
-    });
+    }, "224262811");
     console.log(supply);
 }
 
