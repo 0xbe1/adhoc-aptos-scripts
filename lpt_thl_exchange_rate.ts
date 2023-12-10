1fc4fe59ffb56a2b99f4824a1828d295e265315c
0xbe1 <0xbetrue@gmail.com>
2023-12-10 21:45:17 +0800

    prettier

--- lpt_thl_exchange_rate.ts
+++ lpt_thl_exchange_rate.ts
@@ -1,4 +1,4 @@
-import { AptosClient } from 'aptos';
+import { AptosClient } from "aptos";
 
 /**
  *  public fun one_lpt_thl_amount(): u64 {
@@ -9,35 +9,57 @@ import { AptosClient } from 'aptos';
     }
  */
 
-const THALASWAP_CONTRACT = "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af";
-const client = new AptosClient('https://fullnode.mainnet.aptoslabs.com/v1');
+const THALASWAP_CONTRACT =
+  "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af";
+const client = new AptosClient("https://fullnode.mainnet.aptoslabs.com/v1");
 
 async function main() {
-    for (let i = 224200000; i >= 114200000; i = i - 1000000) {
-        let block = await client.getBlockByVersion(i);
-        let redeemed = await getRedeemdedTHL(i);
-        console.log(`(${i})[${new Date(Number(block.block_timestamp) / 1000).toLocaleString('en-US')}] ${redeemed.toFixed(3)}`)
-    }
+  for (let i = 224200000; i >= 114200000; i = i - 1000000) {
+    let block = await client.getBlockByVersion(i);
+    let redeemed = await getRedeemdedTHL(i);
+    console.log(
+      `(${i})[${new Date(Number(block.block_timestamp) / 1000).toLocaleString(
+        "en-US",
+      )}] ${redeemed.toFixed(3)}`,
+    );
+  }
 }
 
 async function getRedeemdedTHL(ledgerVersion: number) {
-    const [supplyString] = await client.view({
-        function: `${THALASWAP_CONTRACT}::base_pool::pool_token_supply`,
-        type_arguments: [`${THALASWAP_CONTRACT}::weighted_pool::WeightedPoolToken<0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD, 0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_20, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_80, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null>`],
-        arguments: []
-    }, ledgerVersion.toString());
-    const supply = Number(supplyString);
+  const [supplyString] = await client.view(
+    {
+      function: `${THALASWAP_CONTRACT}::base_pool::pool_token_supply`,
+      type_arguments: [
+        `${THALASWAP_CONTRACT}::weighted_pool::WeightedPoolToken<0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD, 0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_20, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_80, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null, 0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null>`,
+      ],
+      arguments: [],
+    },
+    ledgerVersion.toString(),
+  );
+  const supply = Number(supplyString);
 
-    const [poolBalances] = await client.view({
-        function: `${THALASWAP_CONTRACT}::weighted_pool::pool_balances_and_weights`,
-        type_arguments: ["0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD", "0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_20", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_80", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null", "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null"],
-        arguments: []
-    }, ledgerVersion.toString());
-    const thlBalance = Number(poolBalances[1]);
+  const [poolBalances] = await client.view(
+    {
+      function: `${THALASWAP_CONTRACT}::weighted_pool::pool_balances_and_weights`,
+      type_arguments: [
+        "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD",
+        "0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_20",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_80",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null",
+        "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null",
+      ],
+      arguments: [],
+    },
+    ledgerVersion.toString(),
+  );
+  const thlBalance = Number(poolBalances[1]);
 
-    // LPT to redeem / LPT supply = THL to redeem / THL balance
-    // => THL to redeem = LPT to redeem * THL balance / LPT supply
-    return thlBalance / supply;
+  // LPT to redeem / LPT supply = THL to redeem / THL balance
+  // => THL to redeem = LPT to redeem * THL balance / LPT supply
+  return thlBalance / supply;
 }
 
 main();
